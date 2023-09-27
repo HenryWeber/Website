5bb80010829455d598c1a94d7fc8e8340c5169cf
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2023-09-27 12:23:41 -0500

    Add files via upload

---
+++ LoginApplication/home.php
@@ -0,0 +1,31 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {//The isset() function will check if a particular variable is declared and therefore prevent such errors from occurring, especially when dealing with HTTP methods.
+	header('Location: login.html');
+	exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="style_login.css" rel="stylesheet" type="text/css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+				<a href="profile.php"><i class="button"></i>Profile</a>
+				<a href="logout.php"><i class="button"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+		</div>
+	</body>
+</html>
